56bc6aa1ca28575a2678357a3179ff7a2509f782
amantyagi23 <amantyagi4987@gmail.com>
2024-05-21 16:31:34 +0530

    21may

--- Example.java
+++ Example.java
@@ -1,5 +1,8 @@
 class Example {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        int age = 10;
+        // byte
+        System.out.println("Hello World\n");
     }
 }
---
+++ Main.java
@@ -0,0 +1,8 @@
+class Main {
+    // body
+
+    void run(String[] args) {
+        System.out.println("Hello world");
+    }
+
+}
