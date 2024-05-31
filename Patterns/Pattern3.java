d25728a6f3838b4432e2d43bbe4d0034e2bc3090
amantyagi23 <amantyagi4987@gmail.com>
2024-05-31 18:35:45 +0530

    31may

---
+++ Patterns/Pattern3.java
@@ -0,0 +1,17 @@
+package Patterns;
+
+public class Pattern3 {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if ((j + 1) % 2 == 0) {
+                    System.out.print(j);
+                } else {
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
