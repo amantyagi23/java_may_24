6946c6549f67a7756db455a900b350e512b13a4e
amantyagi23 <amantyagi4987@gmail.com>
2024-06-01 18:29:27 +0530

    1june

---
+++ Patterns/Pattern7.java
@@ -0,0 +1,14 @@
+package Patterns;
+
+public class Pattern7 {
+    public static void main(String[] args) {
+        int n = 5;
+        char a = 'A';
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print((char) (a + j));
+            }
+            System.out.println();
+        }
+    }
+}
